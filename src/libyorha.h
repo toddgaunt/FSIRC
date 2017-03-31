06de94e153b8066e44acf69cf2ae2e58c4d101bb
Todd Gaunt <toddgaunt@protonmail.ch>
2017-03-31 11:08:05 -0400

    Add doc-string to yorha_readline(), update others

--- src/libyorha.h
+++ src/libyorha.h
@@ -7,13 +7,24 @@
  * Establish a new tcp connection as a server or client.
  *
  * The last argument of this function should be either bind() or connect(). If
- * the function provided is bind(), then 'sockfd' will point to a newly bound
- * tcp socket. If instead connect() is provided, then 'sockfd' will point to a
- * tcp socket connected to the remote host:port.
+ * the function provided is bind(), then 'sockfd' will be bound to a new tcp
+ * socket. If instead connect() is provided, then 'sockfd' will be assigned
+ * tcp socket connected to the 'host':'port'.
+ *
+ * Return: 0 upon success, -1 if there was an error using the sockt.
  */
 int
 yorha_tcpopen(int *sockfd, const char *host, const char *port, 
 		int (*open)(int, const struct sockaddr *, socklen_t));
 
+/**
+ * Read characters from a file descriptor into a buffer.
+ *
+ * Read characters from 'fd' into 'buf' until either 'maxrecv' characters have
+ * been read or a newline character is read. The newline character is included
+ * in the read.
+ * 
+ * Return: Number of characters read into 'buf'.
+ */
 int
 yorha_readline(char *buf, size_t *len, size_t maxrecv, int fd);
